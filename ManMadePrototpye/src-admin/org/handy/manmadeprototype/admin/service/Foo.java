3b6ea3accb1a237561daf8cfa13a60d40bf9fabc
zhuowu.zm <zhuowu.zm@alibaba-inc.com>
2015-12-18 18:52:41 +0800

    添加代码

---
+++ ManMadePrototpye/src-admin/org/handy/manmadeprototype/admin/service/Foo.java
@@ -0,0 +1,7 @@
+package org.handy.manmadeprototype.admin.service;
+
+/**
+ * Created by zhuowu.zm on 2015/12/18.
+ */
+public class Foo {
+}
---
+++ ManMadePrototpye/src-admin/org/handy/manmadeprototype/admin/test.java
@@ -0,0 +1,7 @@
+package org.handy.manmadeprototype.admin;
+
+/**
+ * Created by zhuowu.zm on 2015/12/18.
+ */
+public class test {
+}
