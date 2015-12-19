c580a7c0de0cc82284b384fd02ba5640783aefbd
mizhon <mizhon@live.cn>
2015-12-19 14:09:54 +0800

    添加DAO层获取用户接口代码

---
+++ ManMadePrototpye/src-auth/org/handy/manmadeprototype/auth/dao/ISiteUserMapper.java
@@ -0,0 +1,34 @@
+package org.handy.manmadeprototype.auth.dao;
+
+import org.handy.manmadeprototype.auth.model.SiteUser;
+import org.springframework.security.access.method.P;
+import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by zhongming on 15/12/19.
+ */
+
+@Repository
+public interface ISiteUserMapper {
+
+    /**
+     * 查询用户信息
+     * @param id 用户id
+     * @param username 用户名
+     * @return 网站用户详情列表
+     */
+    List<SiteUser> get(@Param("id") long id, @Param("username") String username);
+
+    Integer insert(@Param("username") String username, @Param("password") String password);
+
+    Integer update(
+            @Param("id") long id,
+            @Param("username") String username,
+            @Param("password") String password,
+            @Param("authorities") String authorities,
+            @Param("enabled") boolean enabled
+    );
+}
